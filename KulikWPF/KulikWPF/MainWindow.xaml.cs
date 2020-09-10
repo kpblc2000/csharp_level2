aab38d94d9bb2b0a32fdd8424771defdbf8492bb
Alexey Kulik <kpblc2000@gmail.com>
2020-09-10 11:09:02 +0300

    Сотрудники и отделы

---
+++ KulikWPF/KulikWPF/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KulikWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class Department
+    {
+        public string DepartName { get; }
+        public Department(string Name)
+        { DepartName = Name; }
+    }
+
+    public class Employee
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public Department depart { get; set; }
+        public Employee(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
